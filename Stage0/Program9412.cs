8b27a4c7d5b3d7ff5b2e3d21cdd835c9ca191b4b
הילה חג'בי <hilachagbi18@gmail.com>
2024-11-04 13:37:07 +0200

    also here

---
+++ Stage0/Program9412.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program
+    {
+         static void welcome9412()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+    
+}
