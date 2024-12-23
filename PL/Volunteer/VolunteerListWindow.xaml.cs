74a467b7aac91a4d4ecdab7b9aa4673cfd294e9e
הילה חג'בי <hilachagbi18@gmail.com>
2024-12-23 11:49:45 +0200

    Stage 5 - Until the end of Chapter 8

--- PL/Volunteer/VolunteerListWindow.xaml.cs
+++ PL/Volunteer/VolunteerListWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BO;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,49 @@ namespace PL.Volunteer
     /// </summary>
     public partial class VolunteerListWindow : Window
     {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
         public VolunteerListWindow()
         {
             InitializeComponent();
+        }
+
+        public IEnumerable<VolunteerInList> VolunteerList
+        {
+            get { return (IEnumerable<VolunteerInList>)GetValue(VolunteerListProperty); }
+            set { SetValue(VolunteerListProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for VolunteerList.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty VolunteerListProperty =
+            DependencyProperty.Register("VolunteerList", typeof(IEnumerable<VolunteerInList>), typeof(VolunteerListWindow), new PropertyMetadata(null));
+
+
+        public BO.VolunteerField SelectedFiled { get; set; }= BO.VolunteerField.All;
+        public BO.VolunteerInList SelectedVolunteer { get; set; }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            VolunteerList = (SelectedFiled == BO.VolunteerField.All) ?
+                s_bl?.volunteer.VolunteerList(null,null)! : s_bl?.volunteer.VolunteerList(null,SelectedFiled)!;
+
+        }
+        private void queryVolunteerList()
+           => VolunteerList = (SelectedFiled == BO.VolunteerField.All) ?
+                s_bl?.volunteer.VolunteerList(null, null)! : s_bl?.volunteer.VolunteerList(null, SelectedFiled)!;
+
+        private void VolunteerListObserver()
+            => queryVolunteerList();
+ 
+       private void Window_Loaded(object sender, RoutedEventArgs e)
+        => s_bl.volunteer.AddObserver(VolunteerListObserver);
+
+        private void Window_Closed(object sender, EventArgs e)
+            => s_bl.volunteer.RemoveObserver(VolunteerListObserver);
+
+        private void DataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
